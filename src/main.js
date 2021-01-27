ebf4b7626554f369d705d9a34ef5c5aa474aaf8d
hubermanophir <77234895+hubermanophir@users.noreply.github.com>
2021-01-27 14:44:44 +0200

    added counter and sort buttons

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 const addButton = document.getElementById("add-button");
 const textInput = document.getElementById("text-input");
-
+const todoList = [];
+//clear input text on click
 addButton.addEventListener("click", (e) => {
     textInput.value = "";
 });
+
