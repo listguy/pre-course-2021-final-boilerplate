67b13f412f48a2d38a4c8539abe95c8d68459e0a
David Bin Neun <davidbinneun@gmail.com>
2021-01-27 19:56:23 +0200

    basic jsonbin.io integration

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ let tasks = [];
 class Task{
     constructor(priority, text){
         this.priority = priority;
-        date = new Date();
+        let date = new Date();
         this.createdAt = date.toLocaleString("SQL");
         this.text = text;
     }
@@ -18,6 +18,23 @@ function addTask(){
     displayTasks();
 }
 
+function sendToServer(){
+    let req = new XMLHttpRequest();
+
+    req.onreadystatechange = () => {
+      if (req.readyState == XMLHttpRequest.DONE) {
+        console.log(req.responseText);
+      }
+    };
+    
+    req.open("PUT", "https://api.jsonbin.io/b/6011936f3126bb747e9fd00f", true);
+    req.setRequestHeader("Content-Type", "application/json");
+    req.send('[{"priority":3,"date":"2021-01-27 18:11:03","text":"apple"},{"priority": 4, "date": "2021-01-26 18:11:03","text": "orange"}]');
+}
+
+sendToServer();
+displayTasks();
+
 function displayTasks(){
 
     let controlSection = document.getElementById("control-section");
@@ -25,27 +42,38 @@ function displayTasks(){
         controlSection.removeChild(controlSection.firstChild);
     }
 
-    for(task of tasks){
-        let todoContainer = document.createElement('div');
-        todoContainer.classList.add('todo-container');
+    let requestURL = 'https://api.jsonbin.io/b/6011936f3126bb747e9fd00f/latest';
+    let request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
 
-        let todoPriority = document.createElement('div');
-        todoPriority.classList.add('todo-priority');
-        todoPriority.append(task.priority);
+    request.onload = function() {
+        let superHeroes = request.response;
+    
+        for(task of superHeroes){
+            let todoContainer = document.createElement('div');
+            todoContainer.classList.add('todo-container');
 
-        let todoCreatedAt = document.createElement('div');
-        todoCreatedAt.classList.add('todo-created-at');
-        todoCreatedAt.append(task.createdAt);
+            let todoPriority = document.createElement('div');
+            todoPriority.classList.add('todo-priority');
+            todoPriority.append(task["priority"]);
+    
+            let todoCreatedAt = document.createElement('div');
+            todoCreatedAt.classList.add('todo-created-at');
+            todoCreatedAt.append(task["date"]);
+    
+            let todoText = document.createElement('div');
+            todoText.classList.add('todo-text');
+            todoText.append(task["text"]);
+            
+            todoContainer.append(todoPriority);
+            todoContainer.append(todoCreatedAt);
+            todoContainer.append(todoText);
 
-        let todoText = document.createElement('div');
-        todoText.classList.add('todo-text');
-        todoText.append(task.text);
-        
-        todoContainer.append(todoPriority);
-        todoContainer.append(todoCreatedAt);
-        todoContainer.append(todoText);
-
-        controlSection.append(todoContainer);
+            controlSection.append(todoContainer);
+        }
+    
     }
 }
 
@@ -59,20 +87,3 @@ function refreshCounter(){
     counter.innerText = "";
     counter.append(tasks.length);
 }
-
-function getData(){
-    let req = new XMLHttpRequest();
-
-    req.onreadystatechange = () => {
-      if (req.readyState == XMLHttpRequest.DONE) {
-        console.log(req.responseText);
-      }
-    };
-    
-    req.open("GET", "https://api.jsonbin.io/b/6011936f3126bb747e9fd00f", true);
-    req.send();
-
-    return req.responseText;
-}
-
-console.log(getData());
