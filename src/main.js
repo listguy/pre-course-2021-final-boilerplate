afe29ffe1cd84726b29e0e5b52cc5f356c03995e
IlayTissona <ilay211@gmail.com>
2021-01-27 20:32:58 +0200

    really really basic css

--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@ async function main() {
 	const API_KEY = "https://api.jsonbin.io/b/601189173126bb747e9fcc1e";
 	const viewSection = document.querySelector("#view-section");
 	const textInput = document.querySelector("#text-input");
-	const tasksArray = await fetch(API_KEY + "/latest").then((res) => res.json());
+	const tasksArray =
+		(await fetch(API_KEY + "/latest").then((res) => res.text())) !== "[{}]"
+			? await fetch(API_KEY + "/latest").then((res) => res.json())
+			: [];
+	console.log(await fetch(API_KEY + "/latest").then((res) => res.json()));
 	console.log(tasksArray);
 	// const tasksArray =
 	// 	localStorage.getItem("my-todo") !== null
