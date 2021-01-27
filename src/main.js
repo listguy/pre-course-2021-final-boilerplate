2fe6b1e8753fdae973b4ff9efd9d155f940f3d59
hubermanophir <77234895+hubermanophir@users.noreply.github.com>
2021-01-27 21:00:24 +0200

    added save list locally

--- src/main.js
+++ src/main.js
@@ -4,88 +4,81 @@ const numberOfTodo = document.getElementById("counter");
 const viewSection = document.getElementById("view");
 const counter = document.getElementById("counter");
 let storedCounter = localStorage.getItem("counter");
+let localJson = localStorage.getItem("json list");
 const todoList = [];
 let inputValue;
-let jsonCounter = 0;
-
-//removes text from input
-addButton.addEventListener('click', e => {
-    textInput.value = "";
-})
+let jsonList = [];
 
 //set counter to stay on refresh
 if (storedCounter) {
-    counter.innerText = storedCounter;
+  counter.innerText = storedCounter;
 }
 //update counter on every click
-addButton.onclick = function() {
-    const key = "counter";
-    let value = Number(counter.innerText);
-    value ++;
-    counter.innerText = Number(counter.innerText) + 1;
-    localStorage.setItem(key, value);
+addButton.onclick = function () {
+  const key = "counter";
+  let value = Number(counter.innerText);
+  value++;
+  counter.innerText = Number(counter.innerText) + 1;
+  localStorage.setItem(key, value);
+};
+
+//saves the list locally using JSON
+if (typeof localJson === "string") {
+  jsonList = JSON.parse(localJson);
 }
 
-// the add button
-// addButton.addEventListener("click", (e) => {
-//     inputValue = textInput.value;  
-//     const inputObject = convertValueToObject(inputValue);
-//     const todoJson = JSON.stringify(inputObject);
-
-//     todoList.push(inputObject);
-//     viewSection.append(itemObjectToDiv(inputObject));
-//     const containers = document.querySelectorAll(".todo-container");
-//     console.log(containers);
-// });
-
-// addButton.addEventListener('click', e => {
-//     inputValue = textInput.value;
-//     const todoJson = JSON.stringify(convertValueToObject(inputValue));
-//     console.log(todoJson);
-//     localStorage.setItem("json"+jsonCounter, todoJson);
-// })
+addButton.addEventListener("click", (e) => {
+  inputValue = textInput.value;
+  const todoJson = JSON.stringify(convertValueToObject(inputValue));
+  jsonList.push(todoJson);
+  localStorage.setItem("json list", JSON.stringify(jsonList));
+});
 
 
 
+
+//removes text from input
+addButton.addEventListener("click", (e) => {
+  textInput.value = "";
+});
 
 function convertValueToObject(value) {
   const current = new Date();
   const creationTime = current.toLocaleString();
   const priority = document.getElementById("priority-selector").value;
   const myTodoItem = {
-    "text": value,
-    "date": creationTime,
-    "priority": priority,
+    text: value,
+    date: creationTime,
+    priority: priority,
   };
   return myTodoItem;
 }
 
 //converts the todo item object into a div container
 function itemObjectToDiv(myTodoItem) {
-    const todoContainer = document.createElement("div");
-    const todoPriority = document.createElement("div");
-    const todoCreatedAt = document.createElement("div");
-    const todoText = document.createElement("div");
- 
-    todoContainer.setAttribute("class", "todo-container");
-    todoPriority.setAttribute("class", "todo-priority");
-    todoCreatedAt.setAttribute("class", "todo-created-at");
-    todoText.setAttribute("class", "todo-text");
-    
-    todoPriority.innerText = myTodoItem["priority"];
-    todoCreatedAt.innerText = myTodoItem["date"];
-    todoText.innerText = myTodoItem["text"];
-    
-    todoContainer.appendChild(todoPriority);
-    todoContainer.appendChild(todoCreatedAt);
-    todoContainer.appendChild(todoText);
+  const todoContainer = document.createElement("div");
+  const todoPriority = document.createElement("div");
+  const todoCreatedAt = document.createElement("div");
+  const todoText = document.createElement("div");
 
-    return todoContainer;
+  todoContainer.setAttribute("class", "todo-container");
+  todoPriority.setAttribute("class", "todo-priority");
+  todoCreatedAt.setAttribute("class", "todo-created-at");
+  todoText.setAttribute("class", "todo-text");
+
+  todoPriority.innerText = myTodoItem["priority"];
+  todoCreatedAt.innerText = myTodoItem["date"];
+  todoText.innerText = myTodoItem["text"];
+
+  todoContainer.appendChild(todoText);
+  todoContainer.appendChild(todoCreatedAt);
+  todoContainer.appendChild(todoPriority);
+
+  return todoContainer;
 }
 
-// //creating a div and adding class 
+// //creating a div and adding class
 // function elementAndClass(divName, className) {
 //     divName = document.createElement("div");
 //     divName.setAttribute('class', className);
 // }
-
