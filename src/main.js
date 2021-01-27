4e1cbeec37c64b63bda0adec329721ece0e787d4
IlayTissona <ilay211@gmail.com>
2021-01-27 15:15:54 +0200

    changed structure to object
    changed addTask function to work with an object, so that it is easier to control the overall data later.

--- src/main.js
+++ src/main.js
@@ -3,28 +3,38 @@ function main() {
 	const controlSection = document.querySelector("#control-section");
 	const viewSection = document.querySelector("#view-section");
 	const textInput = document.querySelector("#text-input");
-
+	const tasksArray = [];
 	const prioritySelector = document.querySelector("#priority-selector");
 	const addButton = document.querySelector("#add-button");
 	addButton.addEventListener("click", newTask);
 
 	function newTask(event) {
-		const toDoContainerDiv = document.createElement("div");
-		toDoContainerDiv.className = "todo-container";
-		viewSection.appendChild(toDoContainerDiv); //creates a new div and inserts it to view section.
-		const toDoText = document.createElement("div");
-		toDoText.className = "todo-text";
-		toDoText.innerText = textInput.value;
-		console.log(textInput);
-		console.log(toDoText);
-		const toDoPriority = document.createElement("div");
-		toDoPriority.className = "todo-priority";
-		toDoPriority.innerText = prioritySelector.value;
-		const toDoDate = document.createElement("div");
-		toDoDate.className = "todo-created-at";
-		toDoDate.innerText = new Date();
-		toDoContainerDiv.appendChild(toDoPriority);
-		toDoContainerDiv.appendChild(toDoDate);
-		toDoContainerDiv.appendChild(toDoText);
+		const toDoContainer = {
+			priority: prioritySelector.value,
+			text: textInput.value,
+			date: new Date(),
+		};
+		textInput.value = ""; //resets input field
+		tasksArray.push(toDoContainer);
+		printTask(toDoContainer, viewSection);
+		console.log(tasksArray);
 	}
+}
+
+function printTask(toDoContainer, viewSection) {
+	const toDoContainerDiv = document.createElement("div");
+	toDoContainerDiv.className = "todo-container";
+	viewSection.appendChild(toDoContainerDiv); //creates a new div and inserts it to view section.
+	const toDoText = document.createElement("div");
+	toDoText.className = "todo-text"; //creates new div for to do text
+	toDoText.innerText = toDoContainer.text; // gives it the inner text of the input value
+	const toDoPriority = document.createElement("div");
+	toDoPriority.className = "todo-priority";
+	toDoPriority.innerText = toDoContainer.priority;
+	const toDoDate = document.createElement("div");
+	toDoDate.className = "todo-created-at";
+	toDoDate.innerText = toDoContainer.date;
+	toDoContainerDiv.appendChild(toDoPriority);
+	toDoContainerDiv.appendChild(toDoDate);
+	toDoContainerDiv.appendChild(toDoText);
 }
