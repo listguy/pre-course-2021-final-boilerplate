6ada7d6a1838f78887986930e46ff22be948c643
IlayTissona <ilay211@gmail.com>
2021-01-28 16:29:18 +0200

    added delete option

--- src/main.js
+++ src/main.js
@@ -7,15 +7,10 @@ async function main() {
 		"https://api.jsonbin.io/v3" + BIN_ID + "/latest"
 	).then((res) => res.json());
 
-	console.log(fetchResponse.record["my-todo"]);
 	const tasksArray =
 		JSON.stringify(fetchResponse.record["my-todo"]) !== `[{}]`
 			? fetchResponse.record["my-todo"]
 			: [];
-	// console.log(
-	// 	await fetch("https://api.jsonbin.io/v3" + BIN_ID.then((res) => res.json())
-	// );
-	console.log(tasksArray);
 	// const tasksArray =
 	// 	localStorage.getItem("my-todo") !== null
 	// 		? JSON.parse(localStorage.getItem("my-todo"))
@@ -26,6 +21,7 @@ async function main() {
 	sortButton.sorted = false;
 	updateCounter(tasksArray);
 	for (let task of tasksArray) {
+		task.index = tasksArray.indexOf(task); //adds index proprty for use in other functions.
 		printTask(task, viewSection);
 	}
 	addButton.addEventListener("click", newTask);
@@ -40,13 +36,7 @@ async function main() {
 		tasksArray.push(toDoContainer);
 		printTask(toDoContainer, viewSection);
 		updateCounter(tasksArray);
-		await fetch("https://api.jsonbin.io/v3" + BIN_ID, {
-			method: "put",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: `{"my-todo": ${JSON.stringify(tasksArray)}}`,
-		});
+		updateJSONBin(tasksArray);
 		// localStorage.clear();
 		// localStorage.setItem("my-todo", JSON.stringify(tasksArray));
 	}
@@ -72,45 +62,85 @@ async function main() {
 		}
 		sortButton.sorted = !sortButton.sorted;
 	});
-}
 
-function printTask(toDoContainer, viewSection) {
-	const toDoContainerDiv = document.createElement("div");
-	toDoContainerDiv.className = "todo-container";
-	viewSection.appendChild(toDoContainerDiv); //creates a new div and inserts it to view section.
-	const toDoText = document.createElement("div");
-	toDoText.className = "todo-text"; //creates new div for to do text
-	toDoText.innerText = toDoContainer.text; // gives it the inner text of the input value
-	const toDoPriority = document.createElement("div");
-	toDoPriority.className = `todo-priority priority${toDoContainer.priority}`;
-	toDoPriority.innerText = toDoContainer.priority;
-	const toDoDate = document.createElement("div");
-	toDoDate.className = "todo-created-at";
-	toDoDate.innerText = datePrinter(new Date(toDoContainer.date));
-	toDoContainerDiv.appendChild(toDoPriority);
-	toDoContainerDiv.appendChild(toDoText);
-	toDoContainerDiv.appendChild(toDoDate);
-}
-
-function updateCounter(tasksArray) {
-	const counter = document.querySelector("#counter");
-	counter.innerHTML = tasksArray.length;
-}
-
-function datePrinter(date) {
-	let out = {
-		year: date.getYear() - 100,
-		month: date.getMonth() + 1,
-		day: date.getDate(),
-		hours: date.getHours(),
-		minutes: date.getMinutes(),
-	};
-	console.log(out);
-	for (let number in out) {
-		if (Number(out.number) < 10) {
-			out.number = "0" + out.number;
-			console.log(number);
-		}
+	function printTask(toDoContainer, viewSection) {
+		const toDoContainerDiv = document.createElement("div");
+		toDoContainerDiv.className = "todo-container";
+		viewSection.appendChild(toDoContainerDiv); //creates a new div and inserts it to view section.
+		const toDoText = document.createElement("div");
+		toDoText.className = "todo-text"; //creates new div for to do text
+		toDoText.innerText = toDoContainer.text; // gives it the inner text of the input value
+		const toDoPriority = document.createElement("div");
+		toDoPriority.className = `todo-priority priority${toDoContainer.priority}`;
+		toDoPriority.innerText = toDoContainer.priority;
+		const toDoDate = document.createElement("div");
+		toDoDate.className = "todo-created-at";
+		toDoDate.innerText = datePrinter(new Date(toDoContainer.date));
+		toDoContainerDiv.appendChild(toDoPriority);
+		toDoContainerDiv.appendChild(toDoText);
+		toDoContainerDiv.appendChild(toDoDate);
+		toDoContainerDiv.index = toDoContainer.index;
 	}
-	return `${out.day}/${out.month}/${out.year} at ${out.hours}:${out.minutes}`;
+
+	function updateCounter(tasksArray) {
+		const counter = document.querySelector("#counter");
+		counter.innerHTML = tasksArray.length;
+	}
+
+	function datePrinter(date) {
+		let out = {
+			year: date.getYear() - 100,
+			month: date.getMonth() + 1,
+			day: date.getDate(),
+			hours: date.getHours(),
+			minutes: date.getMinutes(),
+		};
+		for (let number in out) {
+			if (Number(out.number) < 10) {
+				out.number = "0" + out.number;
+				console.log(number);
+			}
+		}
+		return `${out.day}/${out.month}/${out.year} at ${out.hours}:${out.minutes}`;
+	}
+
+	async function updateJSONBin(tasksArray) {
+		await fetch("https://api.jsonbin.io/v3" + BIN_ID, {
+			method: "put",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: `{"my-todo": ${JSON.stringify(tasksArray)}}`,
+		});
+	}
+
+	document.addEventListener("mouseover", (event) => {
+		if (event.target.classList[0] === "todo-priority") {
+			const priority = event.target;
+			const tipWindow = document.querySelector("#tip-window");
+			tipWindow.style.left =
+				priority.getBoundingClientRect().left +
+				priority.getBoundingClientRect().width / 2 -
+				50 +
+				`px`;
+			tipWindow.style.top = priority.getBoundingClientRect().top - 16 + "px";
+			tipWindow.hidden = false;
+			event.target.addEventListener("click", (clickEvent) => {
+				const containerDiv = clickEvent.target.parentNode;
+				tasksArray.splice(containerDiv.index, 1);
+				containerDiv.hidden = true;
+				updateCounter(tasksArray);
+				updateJSONBin(tasksArray);
+				console.log(tasksArray);
+			});
+		}
+	});
+	document.addEventListener("mouseout", (event) => {
+		if (event.target.classList[0] === "todo-priority") {
+			const priority = event.target;
+			const tipWindow = document.querySelector("#tip-window");
+
+			tipWindow.hidden = true;
+		}
+	});
 }
