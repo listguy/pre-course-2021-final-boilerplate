17231d844a33cd03368edc5070aefab01096bbba
hubermanophir <77234895+hubermanophir@users.noreply.github.com>
2021-01-29 13:55:51 +0200

    added a function to empty the list

--- src/extras.js
+++ src/extras.js
@@ -3,21 +3,42 @@ const containers = document.getElementsByClassName("todo-container");
 
 document.addEventListener("click", (e) => {
   if (e.target.className === "delete") {
-    if(confirm("this is delete")){
-        alert("item deleted")
+    if (confirm("this is delete")) {
+      alert("item deleted");
     }
   }
 });
 
-document.addEventListener("click", e => {
-    if(e.target.id === "delete-all") {
-        deleteAll();
-    }
-})
-
-function deleteAll() {
-  for (let i = 0; i < containers.length; i++) {
-    let Child =  containers[0];
-    viewSection.removeChild(Child);
+document.addEventListener("click", (e) => {
+  if (e.target.id === "delete-all") {
+    deleteAllDivs();
+    emptyJsonbin();
+    counter.innerText = 0;
   }
+});
+
+//deletes all the divs!!
+function deleteAllDivs() {
+  while (viewSection.firstChild) {
+    viewSection.removeChild(viewSection.firstChild);
+  }
+}
+
+async function emptyJsonbin() {
+  await fetch("https://api.jsonbin.io/v3/b/6013b6761de5467ca6bdb0ce", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Master-Key":
+        "$2b$10$5P7RliLTaANiyqYHfkRvWepKYlCjfoARhVbWxxlqCTwQexhfzjuES",
+    },
+    body: JSON.stringify({ "my-todo": [] }),
+  })
+    .then((response) => response.json())
+    .then(({ "my-todo": [] }) => {
+      console.log("Success:", { "my-todo": [] });
+    })
+    .catch((error) => {
+      console.error("Error:", { "my-todo": [] });
+    });
 }
