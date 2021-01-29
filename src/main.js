7530dc7ad2a98afdcc15e7917476373cb1e62a93
Inbar Weinberg <55811314+Inbar-Weinberg@users.noreply.github.com>
2021-01-29 19:43:35 +0200

    completed basic appending of Json
    completed basic appending of tasklist fetched from jsonbin

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@
 "use strict"
 let todoListJson;
 let body = document.body;
+let view = document.getElementById("View");
 pullTodoList(todoListJson);
-insertTodoList(todoListJson);
 
 async function pullTodoList(todoListJson) {
     let response = await fetch("https://api.jsonbin.io/b/60143f61ef99c57c734b9e06");
@@ -15,8 +15,36 @@ async function pullTodoList(todoListJson) {
     }
 }
 
-function insertTodoList(todoListJson){
-    
+function insertTodoList(todoListJson) {
+    if (todoListJson === {}){
+        alert ("empty list");
+        return;
+    }
+    for (let todo of todoListJson) {
+
+        let textContainer = document.createElement("div");
+        textContainer.classList.add("todo-text");
+        let text = todo.text;
+        textContainer.append(text);
+
+        let dateContainer = document.createElement("div");
+        dateContainer.classList.add("todo-created-at");
+        let date = new Date(todo.date);
+        dateContainer.append(date);
+
+
+        let priorityContainer = document.createElement("div");
+        priorityContainer.classList.add("todo-priority");
+        let priority = todo.priority;
+        priorityContainer.append(priority);
+
+        let todoContainer = document.createElement("div");
+        todoContainer.classList.add("todo-container");
+        todoContainer.append(priorityContainer, dateContainer, textContainer);
+
+        view.append(todoContainer);
+
+    }
 }
 
 
