d5dd9e4d772f7dc3192f32c0a802827f2519ef29
Inbar Weinberg <55811314+Inbar-Weinberg@users.noreply.github.com>
2021-01-29 19:14:05 +0200

    basic fetch function working
    first successful  fetch from jsonbin

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
+
 "use strict"
+let todoListJson;
+let body = document.body;
+pullTodoList(todoListJson);
+insertTodoList(todoListJson);
+
+async function pullTodoList(todoListJson) {
+    let response = await fetch("https://api.jsonbin.io/b/60143f61ef99c57c734b9e06");
+    if (response.ok) {
+        todoListJson = await response.json();
+        insertTodoList(todoListJson);
+    } else {
+        alert("HTTP-Error: " + response.status);
+    }
+}
+
+function insertTodoList(todoListJson){
+    
+}
+
+
+
