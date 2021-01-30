cda48f4013953c3e384aab2c4872758277bf8edc
Inbar Weinberg <55811314+Inbar-Weinberg@users.noreply.github.com>
2021-01-30 21:58:59 +0200

    added "delete all" button

--- src/main.js
+++ src/main.js
@@ -10,20 +10,22 @@ async function pullTodoListFromWeb() {
     let response = await fetch("https://api.jsonbin.io/b/60143f61ef99c57c734b9e06/latest ");
     if (response.ok) {
         taskList = await response.json();
-        for (let task of taskList) {
-            insertTaskToHtml(task)
+        if (taskList[0] != false) {
+            for (let task of taskList) {
+                insertTaskToHtml(task)
+            }
         }
     } else {
         alert("HTTP-Error: " + response.status);
     }
 }
 
-let taskForm = document.getElementById("add-task");
+let taskForm = document.getElementById("add-task-form");
 taskForm.addEventListener("submit", addNewTask);
 
 function addNewTask(event) {
     let text = document.getElementById("text-input").value;
-    let priority = document.getElementById("priority-selector").value;
+    let priority = document.getElementById("priority-list").value;
     let task = insertTaskToTaskArray(text, priority);
     insertTaskToHtml(task);
     uploadJson();
@@ -37,7 +39,6 @@ function insertTaskToTaskArray(text, priority, date = new Date()) {
         date
     });
     return taskList[taskList.length - 1];
-
 }
 
 function insertTaskToHtml(task) {
@@ -45,9 +46,9 @@ function insertTaskToHtml(task) {
     let date = (task.date instanceof Date) ? task.date : new Date(task.date);
     let priority = task.priority;
 
-    let containerTemplate=document.querySelector("[data-template]");
+    let containerTemplate = document.querySelector("[data-template]");
     let todoContainer = containerTemplate.cloneNode(true);
-        todoContainer.removeAttribute("data-template");
+    todoContainer.removeAttribute("data-template");
 
 
     let textContainer = todoContainer.querySelector(".todo-text")
@@ -71,9 +72,25 @@ function uploadJson() {
         body: JSON.stringify(taskList),
     })
         .then(response => response.json())
-        .then(null,error => {
+        .then(null, error => {
             alert('Error:', error);
         })
+}
+
+taskForm.addEventListener("click", eraseAll);
+function eraseAll(event) {
+    let deleteBtn = document.getElementById("delete-button")
+    if (event.target != deleteBtn)
+        return;
+    let answer = confirm("Are you sure you want to delete all existing tasks on the list?");
+    if (answer) {
+        taskList = [false];
+        uploadJson();
+        let tasks = document.querySelectorAll(".todo-container");
+        for (let task of tasks) {
+            task.remove();
+        }
+    }
 }
 
 
