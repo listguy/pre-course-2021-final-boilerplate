8fd522bd285ea2dba92e2a7a36ce7d5f0a1080ce
IlayTissona <ilay211@gmail.com>
2021-02-17 09:48:09 +0200

    added get method

--- backend/index.js
+++ backend/index.js
@@ -5,4 +5,14 @@ const FS = require("fs");
 
 DB.use(express.json());
 
+DB.get("/b/:fileName", (req, res) => {
+  FS.readFile(`./${req.params.fileName}.json`, {}, (err, data) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(data);
+    }
+  });
+});
+
 DB.listen(port);
